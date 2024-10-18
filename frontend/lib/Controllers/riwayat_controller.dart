3c0771f9d16924392ab59d2ffd28e8059fc89d35
Rasyad Bimasatya <rasyadbimasatya04@gmail.com>
2024-10-18 10:58:14 +0800

    clean the project

--- frontend/lib/Controllers/riwayat_controller.dart
+++
@@ -1,9 +0,0 @@
-class RiwayatController {
-  void lihatRiwayat() {
-    // Logika untuk melihat riwayat deteksi
-  }
-
-  void lihatDetailRekomendasi(String idDeteksi) {
-    // Logika untuk melihat detail rekomendasi berdasarkan idDeteksi
-  }
-}
