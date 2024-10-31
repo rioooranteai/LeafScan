8a22b5ff09d46ac8fdea43f731c387fa769c98a8
Rasyad Bimasatya <rasyadbimasatya04@gmail.com>
2024-10-31 17:12:37 +0800

    add some addition in some condition when clicked detect now

--- lib/Views/deteksi_penyakit_view.dart
+++ lib/Views/deteksi_penyakit_view.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field, no_leading_underscores_for_local_identifiers, unused_element, avoid_print, use_super_parameters, prefer_const_constructors_in_immutables, library_private_types_in_public_api
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field, no_leading_underscores_for_local_identifiers, unused_element, avoid_print, use_super_parameters, prefer_const_constructors_in_immutables, library_private_types_in_public_api, use_build_context_synchronously, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
+
+import 'dart:ui';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +34,172 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
   late CameraController _controller;
   bool _isCameraInitialized = false;
   File? imageLeaf;
+  bool _isImageCropped = false;
+  bool _isImageDetected = false;
+  bool _isLoading = false;
+  bool _isHealthy = false;
+  bool _isNonLeafDetected = false;
+
+  Future<void> viewResultDetected(BuildContext context) async {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Color.fromARGB(
+          255, 52, 121, 40), // Sesuaikan warna seperti pada gambar
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25.0),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  'Hasil Deteksi',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Divider(
+                height: 2,
+              ),
+              SizedBox(height: 15),
+              // Text(
+              //   'Daun Sehat',
+              //   style: TextStyle(
+              //     color: Colors.white,
+              //     fontWeight: FontWeight.bold,
+              //     fontSize: 20,
+              //   ),
+              // ),
+              // Text(
+              //   'Tidak terdeteksi adanya penyakit pada daun ini.',
+              //   style: TextStyle(
+              //     color: Colors.white,
+              //     fontSize: 16,
+              //   ),
+              // ),
+              Text(
+                'Penyakit Utama',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              ListTile(
+                leading: Transform.rotate(
+                  angle: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: SizedBox(
+                      width: 100, // lebar gambar
+                      height: 100, // tinggi gambar
+                      child: Image.asset(
+                        'assets/images/blight.png',
+                        fit: BoxFit.cover, // agar gambar pas di dalam kotak
+                      ),
+                    ),
+                  ),
+                ), // Gambar Similar 1
+                title: Text('Hawar Daun',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                subtitle: Text('Exserohilum turcicum',
+                    style: TextStyle(color: Colors.white)),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Lainnya',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              ListTile(
+                leading: Transform.rotate(
+                  angle: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: SizedBox(
+                      width: 100, // lebar gambar
+                      height: 100, // tinggi gambar
+                      child: Image.asset(
+                        'assets/images/gray.png',
+                        fit: BoxFit.cover, // agar gambar pas di dalam kotak
+                      ),
+                    ),
+                  ),
+                ),
+                title: Text('Bercak Daun',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                subtitle: Text('Bipolaris maydis',
+                    style: TextStyle(color: Colors.white)),
+              ),
+              ListTile(
+                leading: Transform.rotate(
+                  angle: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: SizedBox(
+                      width: 100, // lebar gambar
+                      height: 100, // tinggi gambar
+                      child: Image.asset(
+                        'assets/images/rust.png',
+                        fit: BoxFit.cover, // agar gambar pas di dalam kotak
+                      ),
+                    ),
+                  ),
+                ), // Gambar Similar 2
+                title: Text('Karat Daun',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                subtitle: Text('Puccinia sorghi',
+                    style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Fungsi untuk simulasi proses deteksi
+  Future<void> detectImage(BuildContext context) async {
+    setState(() {
+      _isLoading = true; // Menampilkan loading
+    });
+
+    // Simulasi proses deteksi (misalnya memanggil API)
+    await Future.delayed(
+        Duration(seconds: 5)); // Gantikan dengan logika deteksi yang sebenarnya
+
+    setState(() {
+      _isLoading = false; // Menyembunyikan loading setelah proses selesai
+      _isImageDetected = true;
+      _isNonLeafDetected = true;
+    });
+
+    if (!_isNonLeafDetected) {
+      viewResultDetected(context);
+    }
+  }
 
   @override
   void initState() {
@@ -39,21 +207,30 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
     super.initState();
   }
 
-  initCamera() async {
+  void initCamera() async {
     _controller = CameraController(widget.cameras[0], ResolutionPreset.max);
     try {
       await _controller.initialize();
     } catch (e) {
       print('Camera Error: $e');
+      // Menampilkan snackbar untuk menunjukkan error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal menginisialisasi kamera')),
+      );
     }
+
     if (_controller.value.hasError) {
       print(
           'Error saat menginisialisasi kamera: ${_controller.value.errorDescription}');
-      // Anda juga dapat menambahkan dialog atau Snackbar untuk memberi tahu pengguna tentang error ini.
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error saat menginisialisasi kamera')),
+      );
     }
+
     if (!mounted) {
       return;
     }
+
     setState(() {
       _isCameraInitialized = true;
     });
@@ -107,6 +284,7 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
         if (croppedFile != null) {
           setState(() {
             imageLeaf = File(croppedFile.path);
+            _isImageCropped = true;
           });
         }
       } catch (e) {
@@ -156,6 +334,7 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
       if (croppedFile != null) {
         setState(() {
           imageLeaf = File(croppedFile.path);
+          _isImageCropped = true;
         });
       }
     } catch (e) {
@@ -166,18 +345,44 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 52, 121, 40),
       body: Stack(
         children: [
           // Jika kamera telah diinisialisasi, tampilkan CameraPreview
-          Positioned.fill(
-            child: CameraPreview(_controller),
-          ), // Tampilkan loader saat kamera belum siap
-          Center(
-            child: Image.asset(
-              'assets/images/scan.png', // Ganti dengan path gambar logo kamu
-              width: 400, // Atur ukuran gambar sesuai kebutuhan
-              height: 400,
+          if (!_isImageCropped && _isCameraInitialized)
+            Positioned.fill(
+              child: CameraPreview(_controller),
             ),
+          Center(
+            child: _isImageDetected
+                ? (_isHealthy
+                    ? Image.file(
+                        imageLeaf!, // Menampilkan gambar hasil deteksi (daun sehat)
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.contain,
+                      )
+                    : (_isNonLeafDetected
+                        ? Image.asset(
+                            'assets/images/notLeaf.png', // Gambar jika yang dideteksi bukan daun
+                            width: 200,
+                            fit: BoxFit.contain,
+                          )
+                        : Image.asset(
+                            'assets/images/multiple_disease.png', // Gambar jika ada penyakit
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.contain,
+                          )))
+                : (_isImageCropped && imageLeaf != null
+                    ? Image.file(
+                        imageLeaf!, // Menampilkan gambar yang sudah di-crop
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.contain,
+                      )
+                    : Image.asset(
+                        'assets/images/scan.png', // Gambar default jika belum ada gambar
+                        width: 400,
+                        height: 400,
+                      )),
           ),
           // Logo LeafScan di bagian atas
           Positioned(
@@ -209,50 +414,185 @@ class _DeteksiPenyakitViewState extends State<DeteksiPenyakitView> {
               },
             ),
           ),
-          Positioned(
-            bottom: 75,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Ikon untuk memilih gambar dari galeri di sebelah kiri tombol "Detect Now"
-                    IconButton(
-                      icon: Icon(Icons.photo_library,
-                          color: Colors.white, size: 30),
-                      onPressed: pickImageFromGallery,
-                    ),
-                    SizedBox(width: 25),
-                    // Tombol "Detect Now"
-                    ElevatedButton(
-                      onPressed: () {
-                        // Capture and detect disease
-                        captureImage();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(
-                            255, 239, 194, 33), // Warna tombol
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 50,
-                          vertical: 10,
-                        ),
-                      ),
-                      child: Text(
-                        "Ambil Gambar",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+          if (_isImageCropped) // Jika gambar sudah di-crop, tampilkan tombol Kembali di kanan atas
+            Positioned(
+              top: 60,
+              left: 20, // Posisi untuk tombol kembali
+              child: IconButton(
+                icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                onPressed: () {
+                  // Reset state ketika kembali ke layar kamera
+                  setState(() {
+                    _isImageCropped = false;
+                    imageLeaf = null; // Hapus gambar yang sudah di-crop
+                    _isImageDetected = false;
+                  });
+                },
+              ),
             ),
-          ),
+          if (!_isImageCropped) // Jika belum ada gambar yang dipilih, tampilkan tombol Pilih Gambar dan Ambil Gambar
+            Positioned(
+              bottom: 75,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.photo_library,
+                            color: Colors.white, size: 30),
+                        onPressed: pickImageFromGallery,
+                      ),
+                      SizedBox(width: 25),
+                      ElevatedButton(
+                        onPressed: captureImage,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 239, 194, 33),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 50,
+                            vertical: 10,
+                          ),
+                        ),
+                        child: Text(
+                          "Ambil Gambar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          if (_isImageCropped &&
+              !_isNonLeafDetected) // Jika gambar sudah di-crop, tampilkan tombol "Deteksi Sekarang"
+            Positioned(
+              bottom: 50,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () => detectImage(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 239, 194, 33), // Warna kuning
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 10,
+                    ),
+                  ),
+                  child: Text(
+                    "Deteksi Sekarang",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          if (_isImageDetected &&
+              !_isNonLeafDetected) // Jika gambar sudah di-crop, tampilkan tombol "Deteksi Sekarang"
+            Positioned(
+              bottom: 50,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () => viewResultDetected(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 239, 194, 33), // Warna kuning
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 10,
+                    ),
+                  ),
+                  child: Text(
+                    "Lihat Hasil Deteksi",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          if (_isNonLeafDetected &&
+              _isImageDetected) // Jika objek yang terdeteksi bukan daun
+            Positioned(
+              bottom: 200,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Objek yang Dideteksi Bukan Daun\nSilakan Coba Lagi dengan Gambar Daun",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Tambahkan aksi deteksi ulang
+                      setState(() {
+                        _isImageCropped = false;
+                        imageLeaf = null; // Hapus gambar yang sudah di-crop
+                        _isImageDetected = false;
+                        _isNonLeafDetected = false;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Color.fromARGB(255, 239, 194, 33), // Warna kuning
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 10,
+                      ),
+                    ),
+                    child: Text(
+                      "Kembali",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          // Tampilan loading ketika proses deteksi sedang berjalan
+          if (_isLoading)
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), // Efek blur
+                child: Container(
+                  color:
+                      Colors.black.withOpacity(0.2), // Warna semi-transparent
+                  child: Center(
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                        strokeWidth: 8, // Perbesar ukuran loading
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
         ],
       ),
     );
